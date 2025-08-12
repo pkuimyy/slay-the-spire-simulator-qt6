884ba874505d64b03718e0520f639cfad6ca1884
yangy308 <yangy308@chinatelecom.cn>
2025-08-12 14:16:00 +0800

    feat: add first unit test

--- test/unit/play_card.cpp
+++ test/unit/play_card.cpp
@@ -1,4 +1,47 @@
 #include <gtest/gtest.h>
+
+#include <entt/entt.hpp>
+
+#include "attack.h"
+#include "event/attack.h"
+#include "health.h"
+#include "health_system.h"
+
+void playerUseCard(entt::registry& registry, entt::dispatcher& dispatcher, const entt::entity player,
+                   const entt::entity enemy, const entt::entity card) {
+    const auto* attack = registry.try_get<Attack>(card);
+    if (!attack) {
+        std::cout << "Card has no attack component!\n";
+        return;
+    }
+
+    std::cout << "Player uses card dealing " << attack->damage << " damage.\n";
+
+    AttackEvent evt{player, enemy, attack->damage};
+    dispatcher.enqueue(evt);
+}
+
 TEST(EventModule, playCard) {
-    
+    entt::registry registry;
+    entt::dispatcher dispatcher;
+
+    HealthSystem healthSystem(registry);
+    dispatcher.sink<AttackEvent>().connect<&HealthSystem::onAttack>(&healthSystem);
+
+    constexpr int oldHealth = 200;
+    constexpr int attackDamage = 15;
+
+    const auto player = registry.create();
+    registry.emplace<Health>(player, Health{100, 100});
+    const auto enemy = registry.create();
+    registry.emplace<Health>(enemy, Health{oldHealth, 200});
+    const auto card = registry.create();
+    registry.emplace<Attack>(card, Attack{attackDamage});
+
+    playerUseCard(registry, dispatcher, player, enemy, card);
+    dispatcher.update();
+
+    auto* enemyHealth = registry.try_get<Health>(enemy);
+    ASSERT_NE(enemyHealth, nullptr);
+    ASSERT_EQ(enemyHealth->current, oldHealth - attackDamage);
 }
