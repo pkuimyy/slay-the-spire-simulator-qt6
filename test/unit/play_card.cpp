a7e4e6388cb6fa2bf77e8569885c4c29898abf1d
yangy308 <yangy308@chinatelecom.cn>
2025-08-12 14:00:56 +0800

    refac: rename tests sub-project to test

---
+++ test/unit/play_card.cpp
@@ -0,0 +1,4 @@
+#include <gtest/gtest.h>
+TEST(EventModule, playCard) {
+    
+}
--- tests/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <gtest/gtest.h>
-
-TEST(CoreSystemTest, AddsCorrectly) {
-    EXPECT_EQ(2 + 3, 5);
-}
-
-TEST(CoreSystemTest, HandlesZero) {
-    EXPECT_EQ(0 + 0, 0);
-}
