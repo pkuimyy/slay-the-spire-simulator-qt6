3043017a8961f49c028593c8adf500e3673ed957
yangy308 <yangy308@chinatelecom.cn>
2025-08-13 11:25:07 +0800

    fix: set spdlog logger as global variable

--- tui/main.cpp
+++ tui/main.cpp
@@ -1,13 +1,8 @@
 #include "util/logging.h"
 
 int main(int argc, char **argv) {
-    core::initLogging(spdlog::level::debug);
-
     spdlog::debug("Debug log after initLogging");
     spdlog::info("Info log after initLogging");
     spdlog::warn("Warn log after initLogging");
-
-    const auto level = spdlog::default_logger()->level();
-    spdlog::info("Current logger level is {}", spdlog::level::to_string_view(level));
     return 0;
 }
